81e549e92bed20036ca96ccbf2defcb144686bc3
JoelFausto <joelfausto57@gmail.com>
2024-01-30 18:47:38 -0300

    update

---
+++ box_vinil/android/app/src/main/kotlin/com/example/boxvinil/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.boxvinil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- box_vinil/test/widget_test.dart
+++ box_vinil/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:box_vinil/main.dart';
+import 'package:boxvinil/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
